cfe4b56670311bdbec9bf9e6b55a1c751e8dc2e7
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-15 11:51:56 -0300

    Change format from random org to plain

--- src/DeliveryFeeCalculator.php
+++ src/DeliveryFeeCalculator.php
@@ -6,7 +6,7 @@ class DeliveryFeeCalculator
 {
     public function calculateFee(): int
     {
-        $fee = file_get_contents('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=html&rnd=new');
+        $fee = file_get_contents('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new');
 
         return (int)$fee;
     }
