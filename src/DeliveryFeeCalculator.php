006b3f5937c0415efe780c5c342fd9257947a18d
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-15 11:44:55 -0300

    Create DeliveryFeeCalculator class

---
+++ src/DeliveryFeeCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class DeliveryFeeCalculator
+{
+    public function calculateFee(): int
+    {
+        $fee = file_get_contents('https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=html&rnd=new');
+
+        return (int)$fee;
+    }
+}
